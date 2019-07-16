d03de10878e0ee49971ad89049352970c9437c83
Anna Baseley <akbaseley@gmail.com>
2019-07-16 11:12:02 -0400

    Adding Condional Excercise1

---
+++ ConditionalsExercise1/ConditionalsExercise1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConditionalsExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
