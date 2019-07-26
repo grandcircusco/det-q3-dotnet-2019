518268c577d18ce963b77f30fd882ce016280606
stevojed <jedi1014@comcast.net>
2019-07-26 08:41:25 -0400

    Unit Testing Demo

---
+++ UnitTestDemo/UnitTestDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitTestDemo
+{
+    class Program
+    {
+        public static double AddNumbers(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ UnitTestDemo/UnitTestDemo/UnitTestDemoTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTestDemo
+{
+    public class UnitTestDemoTests
+    {
+        [Theory]
+        [InlineData(2,3)]
+        [InlineData(1,21)]
+        [InlineData(7, 71)]
+        [InlineData(435, 345)]
+        [InlineData(245, 789)]
+        public static void AddTwoNumbers(double x, double y)
+        {
+            double expected = x+y;
+            double actual = Program.AddNumbers(x,y);
+
+            Assert.Equal(expected, actual);
+        }    
+    }
+}
