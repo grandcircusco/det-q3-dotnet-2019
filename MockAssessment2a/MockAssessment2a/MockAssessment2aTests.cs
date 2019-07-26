4b1e5920d4956ca73524bbf8540df9cbb9384fc5
stevojed <jedi1014@comcast.net>
2019-07-26 10:02:52 -0400

    First Commit

---
+++ MockAssessment2a/MockAssessment2a/MockAssessment2aTests.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using System;
+
+
+namespace MockAssessment2a
+{
+    public class MockAssessment2aTests
+    {
+        [Fact]
+        public static void AddToArray()
+        {            
+            int actual = Challenge.AddStarWarsCharacters("Luke", "Darth", "Yoda");
+
+            Assert.Equal(2, actual);
+        }
+
+        [Fact]
+        public static void TestOfStrength()
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            dictionary.Add("Yoda", 4500000);
+            dictionary.Add("Darth", 60200);
+            dictionary.Add("Luke", 3000);
+
+            string expected = "Yoda".ToLower();
+
+            string actual = Challenge.DeathStarCombat(dictionary).ToLower();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public static void PlanetReversal()
+        {
+            string[] planeted = {"Tatooine", "Alderaan", "Hoth", "Naboo", "Endor" };
+
+            List<string> expected = new List<string> {"Endor", "Naboo", "Hoth", "Alderaan", "Tatooine" };
+            List<string> actual = Challenge.ConvertPlantets(new List<string>(), planeted);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public static void DroidAverages()
+        {
+            List<int> droids = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 23, 16, 5678, 234, 5, 46, 678, 9, 87, 456, 356, 34, 2 };
+
+
+            double expected = 537.857142857143;           
+
+            double actual = Challenge.AverageDroids(droids);
+
+            Assert.Equal(Math.Round(expected, 2), Math.Round(actual, 2));
+        }
+        
+        [Fact]
+        public static void VaderWasCaptured()
+        {
+            string expected = "Vader Was Captured".ToLower();
+            string actual = Challenge.TryToCatchDarthVader("1").ToLower();       
+            
+
+            Assert.Contains(expected, actual);
+        }
+
+        [Fact]
+        public static void VaderGotAway()
+        {
+            string expected = "Vader Got Away".ToLower();
+            string actual = Challenge.TryToCatchDarthVader("Vader Haters").ToLower();           
+
+            Assert.Contains(expected, actual);
+        }
+    }
+}   
---
+++ MockAssessment2a/MockAssessment2a/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace MockAssessment2a
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //
+        // Welcome to the Star Wars Mock Assessment.
+        //
+        // In order to run the unit tests provided in this project you will need to down a few NuGet Packages.
+        // 1. Select Tools - from the Menu Bar
+        //   a. NuGet Package Manager
+        //   b. Manage NuGet Packages for solution...
+        //
+        // 2. In here you will select the BROWSE tab.
+        //   a. Search for xunit
+        //   b. install xunit (this should be a 41.6M file).
+        //   c. On the right-hand side you will see the project MockAssessment2a. Check this box and 
+        //      hit install.
+        //
+        // 3. Search for xunit.runner.visualstudios ( this should be 33.6M file ).
+        //   a. install. Click "I Accept" to any prompts that pop up.
+        //
+        // 4. Select Extensions - from the Menu Bar (look up! ;) )
+        //   a. Manage Extensions
+        //   b. Navigate to "Online" on the left of the window.
+        //   c. Search for xUnit.net.TestGenerator in the upper right search bar in the window.
+        //   d. Select xUnit.net.TestGenerator and download.
+        //   e. This will require you to restart Visual Studios to install!!
+        //   f. A pop-up window will appear asking if you want to Modify Visual Studios - click Modify.
+        //
+        // 5. Select Build - from the Menu Bar above! Look up! ;)
+        //   a. Clean Solution
+        //   b. Build Solution
+        //
+        //#####################################################################
+        //#                        RUNNING THE TESTS!!                        #
+        //# 1. To run the tests select Test from the Menu Bar above! Look Up! #
+        //#   a. Go to Windows -> Test Explorer                               #
+        //# 2. Click on Run All in the test explorer window that pops up.     #
+        //# 3. See your results!                                              #
+        //#####################################################################
+        //}       
+    }
+}
