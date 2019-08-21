5a657953a6048b1533626fb50162f529dbbb5c4d
Anna Baseley <akbaseley@gmail.com>
2019-08-21 14:30:12 -0400

    EF Core Exercise update

--- Day 28 - EntityFrameworkCore/EFCoreMovieDbFirst/Controllers/MovieController.cs
+++ Day 28 - EntityFrameworkCore/EFCoreMovieDbFirst/Controllers/MovieController.cs
@@ -23,6 +23,38 @@ namespace EFCoreMovieDbFirst.Controllers
             return View(movieList);
         }
 
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddMovie(Movie newMovie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Movie.Add(newMovie);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+        public IActionResult DeleteMovie(int Id)
+        {
+            Movie found = _context.Movie.Find(Id);
+            if(found != null)
+            {
+                _context.Movie.Remove(found);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
       
     }
 }
