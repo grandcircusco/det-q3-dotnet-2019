bc1646445a1ffc6c5e73f31e7e8f410f380cec32
Anna Baseley <akbaseley@gmail.com>
2019-08-23 17:03:34 -0400

    Added EF Core example

---
+++ Day 26 - State Management/WebApplication6/WebApplication6/Controllers/CompanyController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private readonly CompanyDbContext _context;
+
+        public CompanyController(CompanyDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Employee.ToList());
+        }
+
+        public IActionResult DisplayEmployee(int Id)
+        {
+            Employee employee = _context.Employee.Find(Id);
+            return View(employee);
+        }
+    }
+}
---
+++ Day 26 - State Management/WebApplication6/WebApplication6/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication6.Models
+{
+    public partial class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int? DepartmentId { get; set; }
+
+        public virtual Department Department { get; set; }
+    }
+}
