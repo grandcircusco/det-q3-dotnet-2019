9f7ea2765fc389047ec0855b29c901f590b75135
Anna Baseley <akbaseley@gmail.com>
2019-08-28 15:23:14 -0400

    Day 33 API Stuff & gitIgnore introduction

---
+++ Day 33 - WebAPIsDay3/Day 33 - WebAPIsDay3/Controllers/CatAPIMethods.cs
@@ -0,0 +1,40 @@
+﻿using Day_33___WebAPIsDay3.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Day_33___WebAPIsDay3.Controllers
+{
+    public class CatAPIMethods
+    {
+        public static HttpClient GetHttpClient()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.thecatapi.com");
+            var apiKey = ConfigurationManager.AppSettings["CatAPIKey"];
+            client.DefaultRequestHeaders.Add("x-api-key", apiKey);
+            return client;
+        }
+
+        public static async Task<List<CatBreed>> CatBreeds()
+        {
+            var client = GetHttpClient();
+
+            var response = await client.GetAsync("v1/breeds");
+            var catBreeds = await response.Content.ReadAsAsync<List<CatBreed>>();
+            return catBreeds;
+        }
+
+        public static async Task<BreedImage> GetBreedImages()
+        {
+            var client = GetHttpClient();
+
+            var response = await client.GetAsync("v1/images/search?breed_id=beng");
+            var catImages = await response.Content.ReadAsAsync<List<BreedImage>>();
+            return catImages[0];
+        }
+    }
+}
---
+++ Day 33 - WebAPIsDay3/Day 33 - WebAPIsDay3/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Day_33___WebAPIsDay3.Models;
+using System.Net.Http;
+using System.Configuration;
+
+namespace Day_33___WebAPIsDay3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var catBreeds = CatAPIMethods.CatBreeds().Result;
+            return View(catBreeds);
+        }
+
+
+        public IActionResult RandomCatbyBreed()
+        {
+            var catImages = CatAPIMethods.GetBreedImages().Result;
+            return View(catImages);
+        }
+    }
+}
